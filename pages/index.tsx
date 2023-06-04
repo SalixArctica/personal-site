a46c67c964082012894f14b6e2a047854addbaa3
Willow Lancaster <wlancaster@mux.com>
2023-06-04 14:21:13 -0500

    add svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Card from '../components/Card'
 export default function Home() {
   return (
     <PageLayout center>
-        <Card href="" img="logo.png">
+        <Card href="" img="logo.svg">
           <h1>
             Hi, I'm Willow
           </h1>
